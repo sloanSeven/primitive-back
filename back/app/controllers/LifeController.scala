86131778fb5378234e40c8e72ade92fe8dac4cc7
sloanSeven <steve.j.leonard@gmail.com>
2018-03-20 18:50:06 -0400

    Develop (#1)
    * setup for ec2
    
    * makeup
    
    * conway's game of life
    
    * life works
    
    * ignore pem
    
    * ds store
    
    * remove ds_Store
    
    * ds store nonsense
    
    * remove ds_store

---
+++ back/app/controllers/LifeController.scala
@@ -0,0 +1,25 @@
+package controllers
+
+import javax.inject._
+
+import play.api.mvc._
+import services.LifeBoard
+
+/**
+ * This controller demonstrates how to use dependency injection to
+ * bind a component into a controller class. The class creates an
+ * `Action` that shows an incrementing count to users. The [[Board]]
+ * object is injected by the Guice dependency injection system.
+ */
+@Singleton
+class LifeController @Inject() (cc: ControllerComponents,
+                                 d: LifeBoard) extends AbstractController(cc) {
+
+  /**
+   * Create an action that responds with the [[Board]]'s current
+   * count. The result is plain text. This `Action` is mapped to
+   * `GET /count` requests by an entry in the `routes` config file.
+   */
+  def board = Action { Ok(views.html.life(d.nextMove().toString)) }
+
+}
